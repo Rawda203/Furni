c0f8fed0533018b9440043b4ca28b778ebcedb4c
rawda <rawdaezzmostafa123@gmail.com>
2025-09-29 16:07:02 +0300

    update about and home components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as AOS from 'aos';
+import { CommonModule } from '@angular/common';
 import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule], 
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -14,13 +17,20 @@ export class HomeComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    console.log('AOS:', AOS); 
+    console.log('AOS:', AOS);
     AOS.init({
       once: false,
-  mirror: true,
-  duration: 1000,
-  easing: 'ease-in-out',
-  offset: 150
+      mirror: true,
+      duration: 1000,
+      easing: 'ease-in-out',
+      offset: 150
     });
   }
+
+  features = [
+    { icon: 'assets/images/truck.svg', title: 'Fast & Free Shipping', desc: 'Donec vitae odio quis nisl dapibus malesuada.' },
+    { icon: 'assets/images/bag.svg', title: 'Easy to Shop', desc: 'Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor.' },
+    { icon: 'assets/images/support.svg', title: '24/7 Support', desc: 'Aliquam vulputate velit imperdiet dolor tempor tristique.' },
+    { icon: 'assets/images/return.svg', title: 'Hassle Free Returns', desc: 'Donec vitae odio quis nisl dapibus malesuada.' },
+  ];
 }
